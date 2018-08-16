c288aaad0d23704e42679113fbbb1cae8178a901
Leo <leowu2017@gmail.com>
2018-08-17 00:45:55 +0800

    首頁選單指示

--- imports/ui/Header.js
+++ imports/ui/Header.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom'
 
 import '../../public/stylesheets/global.css'
 import '../../public/stylesheets/header.css'
@@ -18,15 +18,23 @@ export const Header = () => (
 );
 */
 
-export const Header = () => (
-    <header>
-		<div className='fix-width-center'>
-			<img id='logo' src='images/logo@2x.png' width='194' height='32' />
-			<div id='header-right'>
-				<a id='FB-link' className='menu-item' href='https://www.facebook.com/ChhoeTaigi/'></a>
-				<Link className='menu-text menu-item menu-item-select' to='/about'>關於找台語</Link>
-				<Link className='menu-text menu-item' to='/'>首頁</Link>
-			</div>
-		</div>
-	</header>
-);
+class Header extends Component {
+    render() {
+        var currentLocation = this.props.location.pathname
+
+        return (
+            <header>
+                <div className='fix-width-center'>
+                    <img id='logo' src='images/logo@2x.png' width='194' height='32' />
+                    <div id='header-right'>
+                        <a id='FB-link' className='menu-item' href='https://www.facebook.com/ChhoeTaigi/'></a>
+                        <Link className={'menu-text menu-item ' + (currentLocation == '/about' ? 'menu-item-select' : '')} to='/about'>關於找台語</Link>
+                        <Link className={'menu-text menu-item ' + (currentLocation == '/' ? 'menu-item-select' : '')} to='/'>首頁</Link>
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default withRouter(Header);
