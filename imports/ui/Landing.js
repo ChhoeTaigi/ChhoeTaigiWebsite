b011db93c49be27876ca06f1ffb19ecf158a2947
Leo <leowu2017@gmail.com>
2018-08-20 21:58:29 +0800

    完成landing page

--- imports/ui/Landing.js
+++ imports/ui/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import '../../public/stylesheets/landing.css';
 
 export default Landing = () => {
     return (
-        <img id='landing' src='images/landing_kv@2x 2.png' width='918' height='387' />
+        <div>
+            <img id='landing' src='images/landingKV@2x.png' width='879' height='346' />
+            <Link id='more' to='/about'>了解更多</Link>
+        </div>
     )
 }
