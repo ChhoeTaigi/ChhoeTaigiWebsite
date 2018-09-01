b743d72bb6408b022cfd8c9a8bd88de7eff950a1
Leo <leowu2017@gmail.com>
2018-09-01 23:19:28 +0800

    加入FB留言

--- imports/ui/Detail.js
+++ imports/ui/Detail.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
 import DicStruct from '../api/dictionary_struct';
 
-export default class Detail extends Component {
+class Detail extends Component {
     constructor (props) {
         super(props);
         let dic = this.props.match.params.dictionary;
@@ -18,6 +20,20 @@ export default class Detail extends Component {
         };
     }
 
+    componentDidMount () {
+        const script = document.createElement("script");
+
+        script.src = `var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=306448440105903&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));`;
+        script.async = true;
+
+        document.getElementById('script').appendChild(script);
+    }
+
     render() {
         let dic = this.state.dic
         let dicStruct = DicStruct.filter(struct => struct.name===dic)[0]
@@ -31,14 +47,20 @@ export default class Detail extends Component {
         }
 
         let chineseName = dicStruct.chineseName;
+        const path = 'https://' + window.location.hostname + this.props.location.pathname;
+        console.log(path);
         return (
             <div>
+                <div id="fb-root"></div>
+                <div id='script'></div>
                 <h2>{chineseName}</h2>
                 <ul>
                     {content}
                 </ul>
+                <div className="fb-comments" data-href={path} data-numposts="5"></div>
             </div>
         );
-
     }
 }
+
+export default withRouter(Detail);
