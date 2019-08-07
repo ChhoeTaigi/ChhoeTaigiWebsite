33c0ea9b265bc6aa482c9baf28b34aeb35979d90
shiami <shiami15@gmail.com>
2019-08-08 00:51:24 +0800

    主畫面免koh登入。

--- imports/ui/main/Main.js
+++ imports/ui/main/Main.js
@@ -16,36 +16,33 @@ import Account from '../pages/Account';
 class Main extends Component {
     render() {
         const detailPath = getDetailPath();
-        let additionalRoute = [];
-        if (Meteor.userId() || env === 'prod') {
-            additionalRoute = [
-                <Route key='basic' exact path='/' component={BasicSearch} />,
-                <Route key='advanced' exact path='/chinkai' component={AdvancedSearch} />,
-                <Route key='explanation' exact path='/annachhoe' component={Explanation} />,
-                <Route key='search' exact path='/search' component={Search} />,
-                <Route key='detail' exact path={detailPath} component={Detail} />,
-                <Route key='about' exact path='/liaukai' component={About} />,
-                <Route key='app' exact path='/app' component={DicApp} />,
-            ];
-            if (Meteor.userId()) {
-                additionalRoute.push(
-                    <Route key='update' exact path='/update' component={Update} />
-                );
-            }
+        let additionalRoute = [
+            <Route key='basic' exact path='/' component={BasicSearch} />,
+            <Route key='advanced' exact path='/chinkai' component={AdvancedSearch} />,
+            <Route key='explanation' exact path='/annachhoe' component={Explanation} />,
+            <Route key='search' exact path='/search' component={Search} />,
+            <Route key='detail' exact path={detailPath} component={Detail} />,
+            <Route key='about' exact path='/liaukai' component={About} />,
+            <Route key='app' exact path='/app' component={DicApp} />,
+        ];
+        if (Meteor.userId()) {
+            additionalRoute.push(
+                <Route key='update' exact path='/update' component={Update} />
+            );
         }
-        
+
         let facebookChat = "";
-        
+
         if (env === 'prod') {
             facebookChat = (
                 // <!-- Your customer chat code -->
                 <div class="fb-customerchat"
-                attribution="install_email"
-                page_id="198185797612542"
-                minimized="true"
-                theme_color="#67b868"
-                logged_in_greeting="平安出頭天！請問kám有問題a̍h是建議？歡迎來留話。"
-                logged_out_greeting="平安出頭天！請問kám有問題a̍h是建議？歡迎來留話。">
+                    attribution="install_email"
+                    page_id="198185797612542"
+                    minimized="true"
+                    theme_color="#67b868"
+                    logged_in_greeting="平安出頭天！請問kám有問題a̍h是建議？歡迎來留話。"
+                    logged_out_greeting="平安出頭天！請問kám有問題a̍h是建議？歡迎來留話。">
                 </div>
             )
         }
@@ -67,5 +64,5 @@ export default withRouter(Main);
 
 function getDetailPath() {
     let dic = DicStruct.map((e) => e.name);
-    return '/:dictionary(' + dic.join('|') +')/:id';
+    return '/:dictionary(' + dic.join('|') + ')/:id';
 }
