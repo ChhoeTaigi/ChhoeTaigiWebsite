53f507adc97035fb3106452f0a3f1fbfd84a5aa5
shiami <shiami15@gmail.com>
2019-08-08 01:51:21 +0800

    修正台日大辭典原冊ê連結，多頁ê顯示。

--- imports/ui/search/components/Word.js
+++ imports/ui/search/components/Word.js
@@ -7,13 +7,36 @@ export default class Word extends Component {
         super(props);
     }
 
+    generateGoanChhehLink = (dicString, pageString) => {
+        let linkHtml = []
+
+        if (dicString === 'TaijitToaSutian') {
+            let pages = pageString.split("/");
+            if (pages.length <= 1) {
+                linkHtml.push(<a href={originalScannedCopy(dicString, pageString)} target='_blank'>{pageString}</a>);
+            } else {
+                for (var i=0; i<pages.length; i++) {
+                    linkHtml.push(<a href={originalScannedCopy(dicString, pages[i])} target='_blank'>{pages[i]}</a>);
+
+                    if (i != pages.length - 1) {
+                        linkHtml.push(", ");
+                    }
+                }
+            }
+        } else {
+            linkHtml.push(<a href={originalScannedCopy(dicString, pageString)} target='_blank'>{pageString}</a>);
+        }
+
+        return linkHtml
+    }
+
     render() {
         const columns = this.props.columns;
         const contents = [];
         for (let key in columns) {
             let content;
             if (key === '原冊頁數')
-                content = <a href={originalScannedCopy(this.props.dic, columns[key])} target='_blank'>{columns[key]}</a>;
+                content = this.generateGoanChhehLink(this.props.dic, columns[key])
             else
                 content = columns[key];   
                 contents.push(
