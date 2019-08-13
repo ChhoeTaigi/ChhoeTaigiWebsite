dfbb12fcb2e0dd2cb39abbddeac764e1d4bb08ca
shiami <shiami15@gmail.com>
2019-08-13 22:06:13 +0800

    Fix DeployTools

--- DeployTools/buildMeteor.sh
+++ DeployTools/buildMeteor.sh
@@ -1,8 +1,8 @@
 cd /home/website/ChhoeTaigiWebsite/
 meteor npm install bcrypt
 meteor npm install
-cd /home/website
 sudo rm -r /home/website/new_package
-meteor build --server-only /home/website/new_package
+cd /home/website
+meteor build --server-only new_package
 sudo chown -R root:dev /home/website/new_package
 sudo chmod 770 -R /home/website/new_package
