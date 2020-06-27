652e2ceec43bd71170325536455866227b043141
shiami <shiami15@gmail.com>
2020-06-28 01:50:02 +0800

    Keng-sin script.

--- DeployTools/deployUpdate.sh
+++ DeployTools/deployUpdate.sh
@@ -5,17 +5,12 @@ sudo rm -rf /var/www/ChhoeTaigi/tmp
 sudo mkdir -p /var/www/ChhoeTaigi/tmp
 cd /var/www/ChhoeTaigi/tmp
 sudo tar xzf /home/website/new_package/ChhoeTaigiWebsite.tar.gz
-# cd /var/www/ChhoeTaigi/tmp/bundle/programs/server
-# sudo npm install --production
-# sudo npm prune --production
 sudo chown -R myappuser: /var/www/ChhoeTaigi/tmp/bundle
 sudo mv /var/www/ChhoeTaigi/bundle /var/www/ChhoeTaigi/bundle.old
 sudo mv /var/www/ChhoeTaigi/tmp/bundle /var/www/ChhoeTaigi/bundle
-sudo -u myappuser -H bash -l
 cd /var/www/ChhoeTaigi/bundle/programs/server
-npm install --production
-exit
-
+sudo node-gyp rebuild -g --unsafe-perm
+sudo npm install --production
 passenger-config restart-app /var/www/ChhoeTaigi/bundle
 sudo rm -rf /var/www/ChhoeTaigi/bundle.old
 cd /home/website/ChhoeTaigiWebsite/DeployTools
