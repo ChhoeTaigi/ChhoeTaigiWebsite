e74d0601a4b3ab7a0d8272a63aa89bdd87b4fa73
shiami <shiami15@gmail.com>
2020-06-28 01:53:56 +0800

    Keng-sin script.

--- DeployTools/deployUpdate.sh
+++ DeployTools/deployUpdate.sh
@@ -9,7 +9,7 @@ sudo chown -R myappuser: /var/www/ChhoeTaigi/tmp/bundle
 sudo mv /var/www/ChhoeTaigi/bundle /var/www/ChhoeTaigi/bundle.old
 sudo mv /var/www/ChhoeTaigi/tmp/bundle /var/www/ChhoeTaigi/bundle
 cd /var/www/ChhoeTaigi/bundle/programs/server
-sudo node-gyp rebuild -g --unsafe-perm
+sudo npm install --unsafe-perm
 sudo npm install --production
 passenger-config restart-app /var/www/ChhoeTaigi/bundle
 sudo rm -rf /var/www/ChhoeTaigi/bundle.old
