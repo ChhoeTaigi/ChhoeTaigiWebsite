7337991da4e2c8a92ff181615c7352c8840c586a
miau715 <miau715@gmail.com>
2021-07-30 16:13:09 +0800

    css refactor 1st round

--- imports/stylesheets/index.js
+++ imports/stylesheets/index.js
@@ -1,18 +1,5 @@
-import './about.scss';
-import './advanced.scss';
-import './all.scss';
-import './app.scss';
-import './basic.scss';
-import './brief-word.scss';
-import './detail.scss';
-import './explanation.scss';
-import './footer.scss';
-import './global.scss';
-import './header.scss';
-import './landing.scss';
-import './single.scss';
-import './word.scss'
+import './styles.scss';
 
 if (Meteor.userId()) {
-    import './manage.css';
+    import './manage.scss';
 }
