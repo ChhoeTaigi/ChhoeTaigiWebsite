4a3089d12bf4554a371d42d133f9515bf1a6fa20
miau715 <miau715@gmail.com>
2021-08-02 19:51:19 +0800

    fix attribute name

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -43,7 +43,7 @@ class App extends Component {
 
     render() {
         return (
-            <div class="content-wrapper">
+            <div className="content-wrapper">
                 <Header />
                 <Main />
                 <Footer />
