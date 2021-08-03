41e9f42d00d51916adceb01cc3599a870d797512
miau715 <miau715@gmail.com>
2021-08-03 18:04:37 +0800

    refactor loading indicator

--- imports/ui/search/components/LoadingIndicator.js
+++ imports/ui/search/components/LoadingIndicator.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
 
 export const LoadingIndicator = (
-    <div id='loading-indicator' className='loading-indicator' style={{width: "100%", height: "100", display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Loader type="ThreeDots" color="#8bbe56" height="100" width="100" />
+    <div id='loading-indicator' className='loading-indicator'>
+        <Loader type="ThreeDots" />
     </div>
 );
